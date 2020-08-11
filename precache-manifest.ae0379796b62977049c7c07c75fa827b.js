97654d30a8559ea343598ec9df99beef52a492d3
tsungtingdu <tsungtingdu@gmail.com>
2020-08-11 17:19:48 +0800

    Updates

---
+++ precache-manifest.ae0379796b62977049c7c07c75fa827b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83b7b9bae54755359f0a1bc8d24f823",
+    "url": "/hotel-page/index.html"
+  },
+  {
+    "revision": "1ec118e2062e9392d896",
+    "url": "/hotel-page/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cbceddea53990b130598",
+    "url": "/hotel-page/static/css/main.97fe906c.chunk.css"
+  },
+  {
+    "revision": "1ec118e2062e9392d896",
+    "url": "/hotel-page/static/js/2.8386019c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hotel-page/static/js/2.8386019c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbceddea53990b130598",
+    "url": "/hotel-page/static/js/main.f524d21c.chunk.js"
+  },
+  {
+    "revision": "7b9161a8f5eb131efb43",
+    "url": "/hotel-page/static/js/runtime-main.924761e4.js"
+  }
+]);
